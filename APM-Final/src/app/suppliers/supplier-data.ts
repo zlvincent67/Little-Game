d9adf83edec0f6e5aba3d244365776f561dba4cc
Lei ZHANG(BGC) <LZhang77@slb.com>
2020-09-06 16:11:52 +0800

    add RxJS

---
+++ APM-Final/src/app/suppliers/supplier-data.ts
@@ -0,0 +1,68 @@
+import { Supplier } from './supplier';
+
+export class SupplierData {
+
+    static suppliers: Supplier[] = [
+        {
+            id: 1,
+            name: 'Gardening Supply',
+            cost: 16.95,
+            minQuantity: 12
+        },
+        {
+            id: 2,
+            name: 'Standard Gardening',
+            cost: 15.95,
+            minQuantity: 24
+        },
+
+        {
+            id: 3,
+            name: 'Gardening Supply',
+            cost: 12,
+            minQuantity: 6
+        },
+        {
+            id: 4,
+            name: 'General Supply',
+            cost: 25,
+            minQuantity: 2
+        },
+        {
+            id: 5,
+            name: 'General Supply',
+            cost: 2,
+            minQuantity: 24
+        },
+        {
+            id: 6,
+            name: 'Tool Supply',
+            cost: 4,
+            minQuantity: 12
+        },
+        {
+            id: 7,
+            name: 'Tools Are Us',
+            cost: 8,
+            minQuantity: 8
+        },
+        {
+            id: 8,
+            name: 'Tool Supply',
+            cost: 4,
+            minQuantity: 12
+        },
+        {
+            id: 9,
+            name: 'Game Supply',
+            cost: 20,
+            minQuantity: 6
+        },
+        {
+            id: 10,
+            name: 'General Supply',
+            cost: 12,
+            minQuantity: 12
+        }
+    ];
+}
